2b0ff6164c491ac4e6d0186e30d685476c48d861
genius-0963 <geniusthegreat0963@gmail.com>
2026-06-10 16:48:49 +0530

    Allow OAuth account linking in auth.ts

--- src/auth.ts
+++ src/auth.ts
@@ -10,6 +10,7 @@ if (process.env.GOOGLE_CLIENT_ID || process.env.AUTH_GOOGLE_ID) {
   providers.push(Google({
     clientId: process.env.GOOGLE_CLIENT_ID || process.env.AUTH_GOOGLE_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET || process.env.AUTH_GOOGLE_SECRET,
+    allowDangerousEmailAccountLinking: true,
   }));
 }
 
@@ -17,6 +18,7 @@ if (process.env.GITHUB_CLIENT_ID || process.env.AUTH_GITHUB_ID) {
   providers.push(GitHub({
     clientId: process.env.GITHUB_CLIENT_ID || process.env.AUTH_GITHUB_ID,
     clientSecret: process.env.GITHUB_CLIENT_SECRET || process.env.AUTH_GITHUB_SECRET,
+    allowDangerousEmailAccountLinking: true,
   }));
 }
 
